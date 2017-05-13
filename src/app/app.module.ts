e0ff602e811121d629b1791ca75cf5e77bb7cf03
Caleb Froese <caleb.froese@gmail.com>
2017-05-13 15:34:20 +0930

    Add timer module and component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,13 +11,14 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HomeComponent} from './components/home/home.component';
+import {TimerModule} from './components/timer/timer.module';
 import {ElectronService} from './providers/electron.service';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule, FormsModule, BrowserAnimationsModule, HttpModule,
-    AppRoutingModule, MaterialModule.forRoot()
+    TimerModule, AppRoutingModule, MaterialModule.forRoot()
   ],
   providers: [ElectronService],
   bootstrap: [AppComponent]
---
+++ src/app/components/timer/timer.module.ts
@@ -0,0 +1,7 @@
+import {NgModule} from '@angular/core';
+
+import {TimerComponent} from './timer.component';
+
+@NgModule({imports: [], declarations: [TimerComponent], exports: [TimerComponent]})
+export class TimerModule {
+}
