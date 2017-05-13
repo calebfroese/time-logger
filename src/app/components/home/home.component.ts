bc8c248cde74fb4e4a3bc58d0627131c70994ca3
Caleb Froese <caleb.froese@gmail.com>
2017-05-13 16:26:08 +0930

    Add flex and timer layout

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  moduleId: module.id,
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
--- src/app/components/timer/timer.module.ts
+++ src/app/components/timer/timer.module.ts
@@ -1,7 +1,12 @@
 import {NgModule} from '@angular/core';
+import {MaterialModule} from '@angular/material';
 
 import {TimerComponent} from './timer.component';
 
-@NgModule({imports: [], declarations: [TimerComponent], exports: [TimerComponent]})
+@NgModule({
+  imports: [MaterialModule],
+  declarations: [TimerComponent],
+  exports: [TimerComponent]
+})
 export class TimerModule {
 }
